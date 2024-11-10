f9057bca3ea5893b98b62aa376184268892b6b21
AmandaRachma <148883739+AmandaRachma@users.noreply.github.com>
2024-11-10 21:15:46 +0700

    Delete lib directory

--- lib/app/modules/home/controllers/home_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class HomeController extends GetxController {
-  var currentIndex = 0.obs; // Reactive index to track the current page
-
-  void changeTab(int index) {
-    currentIndex.value = index;
-  }
-}
